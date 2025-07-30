5fd9785e335272e5b4daeb405154679f7a3ccf92
imamdienul <imamdienulbayan1933@gmail.com>
2025-07-31 01:59:44 +0700

    malam

--- application/views/i_kelas_detail.php
+++ application/views/i_kelas_detail.php
@@ -49,7 +49,7 @@
                                     <th>Nama</th>
                                     <th>Kelas</th>
                                     <th>Kode Presensi Manual</th>
-                                    <th>Foto</th>
+                                    
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
